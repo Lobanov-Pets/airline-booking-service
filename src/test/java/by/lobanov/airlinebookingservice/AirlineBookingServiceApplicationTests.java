03fdc494908dbefd518f2b9dfedddfc3e678e3de
GitLobanov <lobanovgithub@gmail.com>
2025-07-16 11:28:18 +0300

    first commit

---
+++ src/test/java/by/lobanov/airlinebookingservice/AirlineBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lobanov.airlinebookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
